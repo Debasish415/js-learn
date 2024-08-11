904d5334ce2b18e9cf05c9550d3c50363f42ea96
Debasish Maharana <119467591+Debasish415@users.noreply.github.com>
2024-08-11 06:01:24 +0000

    updated

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -1,36 +1,66 @@
-let score = 33
-console.log(typeof score) //------------------------>number type
-console.log(typeof (score))//----------------------->number type
+// let score = 33
+// console.log(typeof score) //------------------------>number type
+// console.log(typeof (score))//----------------------->number type
 
 
 
-let result = "33"
-console.log(typeof result)
-console.log(typeof (result))
-let valueInNumber = Number(result)
-console.log(typeof valueInNumber)
+// let result = "33"
+// console.log(typeof result)
+// console.log(typeof (result))
+// let valueInNumber = Number(result)
+// console.log(typeof valueInNumber)
 
 
-let deba = "33abs"
-let valInNum = Number(deba)
-console.log(typeof valInNum)//---------->number
-console.log(valInNum)
+// let deba = "33abs"
+// let valInNum = Number(deba)
+// console.log(typeof valInNum)//---------->number
+// console.log(valInNum)
 
-//---"33" ----->33
-//---"33abx"---->NaN(but type is number)
-//---true/false----->1/0
+// //---"33" ----->33
+// //---"33abx"---->NaN(but type is number)
+// //---true/false----->1/0
 
-let isLoggedIn = 0;
-let booleanIsLoggedIn = Boolean(isLoggedIn)
-console.log(typeof booleanIsLoggedIn)
-console.log(booleanIsLoggedIn)
+// let isLoggedIn = 0;
+// let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(typeof booleanIsLoggedIn)
+// console.log(booleanIsLoggedIn)
 
-// 1 => true; 0 => false
-// "" => false
-// "hitesh" => true
+// // 1 => true; 0 => false
+// // "" => false
+// // "hitesh" => true
 
-let someNumber = 33
+// let someNumber = 33
 
-let stringNumber = String(someNumber)
-console.log(stringNumber);
-console.log(typeof stringNumber);
+// let stringNumber = String(someNumber)
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
+
+/* --------------------------OPERATIONS---------------------------*/
+let val = 3
+let negval = -val
+console.log(negval)//-------------------------------->  -3
+
+
+let str1 = "hello"
+let str2 = " Deba"
+let str3 = str1 + str2
+console.log(str3)//-----------------------------------> hello Deba
+
+
+console.log("1" + 2) //------------------------------->12
+console.log(1 + "2")//-------------------------------->12
+console.log("1" + 2 + 2)//---------------------------->122
+console.log(1 + 2 + "2")//---------------------------->32
+console.log(1 + "2" + 1) //---------------------------->121
+
+console.log(+true) //---------------------------->1
+console.log(+false) //-------------------------->0
+console.log(+"") //---------------------------->0
+
+let gameCounter = 100
+++gameCounter;
+console.log(gameCounter); //----------------->101
+
+
+
+
